079e2ce937898f18a8920110385f989e33f969bb
Steven Hankin <steven.hankin@gmail.com>
2017-10-31 21:20:07 +0000

    Refactor app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+
+
 $(
     function () {
 
@@ -9,11 +11,26 @@ $(
 
 
         var viewModel = {
-            personName: 'Bob',
-            personAge: 123,
 
-            assignMapCallback: function() {
+            filterInput: ko.observable(''),
+            filterList: ko.observable([]),
 
+            setupAutocomplete: function(map) {
+                // Once we have a map, we can bind the Autocomplete search
+                var input = document.getElementById('search');
+                var options = {strictBounds: true};
+                var searchAutocomplete = new google.maps.places.Autocomplete(input, options);
+                // Only interested in what is within current map
+                searchAutocomplete.bindTo('bounds', map);
+            },
+
+            assignMapCallback: function () {
+
+                var self = this;
+
+                // Called from index.html once both jQuery
+                // and the Google API map library is ready
+                // and is setup in the global scope
                 initMap = function () {
                     // Latitude and longitude of my current home village!
                     var uluru = {lat: 51.1414, lng: 0.5894};
@@ -26,17 +43,16 @@ $(
                         position: uluru,
                         map: model.map
                     });
-                    // Now we have a map, we can bind the Autocomplete search
-                    var input = document.getElementById('search');
-                    var options = {strictBounds: true};
-                    var searchAutocomplete = new google.maps.places.Autocomplete(input,options);
-                    // Only interested in what is within current map
-                    searchAutocomplete.bindTo('bounds', model.map);
+                    self.setupAutocomplete(model.map);
                 };
 
             },
 
-            init: function() {
+            animateMarker: function () {
+                // TODO : Animate marker when clicked directly or via filter list
+            },
+
+            init: function () {
                 this.assignMapCallback();
             }
         };
@@ -46,7 +62,3 @@ $(
 
     }()
 );
-
-
-//
-// https://maps.googleapis.com/maps/api/place/autocomplete/json?input=we&location=51.1414,0.5894&radius=10&strictbounds&key=AIzaSyCCXajBUCO96ptgmu4OnftICZmQrTFcpaY
