fab760d631cd4d167d42157a3b9830090590e8d4
Steven Hankin <steven.hankin@gmail.com>
2017-11-05 23:26:14 +0000

    Refactor and UI Refactored core Marker code and added stepped delay for dropping markers.

--- js/app.js
+++ js/app.js
@@ -1,30 +1,32 @@
 var model = {
 
+    /**
+     * Represents a place of interest
+     * @param name Displayable name of place (appears in Places List)
+     * @param type Determines the marker icon for map
+     * @param coord Latitude and Longitude position of place
+     * @constructor
+     */
     Place: function (name, type, coord) {
-        /**
-         * Represents a place of interest
-         *
-         * name - Displayable name of place (appears in Places List)
-         * type - TODO
-         * coord - Latitude and Longitude position of place
-         */
         this.name = name;
         this.type = type;
         this.coord = coord;
     },
 
+    neighborhoodGeo: {/* This is my neighborhood */ lat: 51.1414, lng: 0.5894},
+
+    /**
+     * Default map configuration
+     * to center over neighborhood
+     * and stylize appropriately
+     */
     mapConfig: {
         zoom: 11,
-        center: {/* This is my neighborhood */ lat: 51.1414, lng: 0.5894},
-        /**
-         * I disable all the usual controls
-         * to make the UI experience cleaner
-         */
         disableDefaultUI: true,
         navigationControl: false,
         mapTypeControl: false,
-        scaleControl: false,
-        draggable: false,
+        // scaleControl: false,
+        // draggable: false,
         styles: [
             {
                 featureType: 'road',
@@ -45,12 +47,12 @@ var model = {
         /**
          * Array of Places used for both the Search and the Map Markers
          */
-        this.places = [new this.Place("My home", 'Home', {lat: 51.138725, lng: 0.591060}),
-            new this.Place("Sissinghurst Castle", 'Castle', {lat: 51.115282, lng: 0.582349}),
-            new this.Place("Scotney Castle", 'Castle', {lat: 51.092852, lng: 0.408211}),
-            new this.Place("Leeds Castle", 'Castle', {lat: 51.248918, lng: 0.630465}),
-            new this.Place("Chapel Down Winery", 'Winery', {lat: 51.040965, lng: 0.699023}),
-            new this.Place("The West House", 'Eatery', {lat: 51.115184, lng: 0.642370})]
+        this.places = [new this.Place("My home", 'home', {lat: 51.138725, lng: 0.591060}),
+            new this.Place("Sissinghurst Castle", 'castle', {lat: 51.115282, lng: 0.582349}),
+            new this.Place("Scotney Castle", 'castle', {lat: 51.092852, lng: 0.408211}),
+            new this.Place("Leeds Castle", 'castle', {lat: 51.248918, lng: 0.630465}),
+            new this.Place("Chapel Down Winery", 'eatery', {lat: 51.040965, lng: 0.699023}),
+            new this.Place("The West House", 'eatery', {lat: 51.115184, lng: 0.642370})]
     }
 };
 
@@ -80,10 +82,142 @@ var gmapViewModel = {
         }, 700)
     },
 
-    createMarkers: function (places) {
-        /**
-         * Create Google Map Markers for supplied places
+    /**
+     * Bespoke Marker icons
+     */
+    icons: {
+        home: {
+            icon: 'static/home-icon.png'
+        },
+        castle: {
+            icon: 'static/castle-icon.png'
+        },
+        eatery: {
+            icon: 'static/restaurant-icon.png'
+        }
+    },
+
+    /**
+     * Create an InfoWindow for the specified Marker
+     * (with supplied place details)
+     * @param marker Marker for infowindow
+     * @param place Place referenced by marker
+     */
+    createInfoWindow: function (marker,place) {
+
+        var self = this;
+
+        self.infowindow.open(self.map, marker);
+        /*
+        Initial message to user whilst an Ajax attempt
+        is made to retrieve a list of images for the place
          */
+        self.infowindow.setContent('Retrieving data..');
+        /*
+        Re-center map to marker, because the infowindow might have
+        large photos that we don't want overspilling top of window
+         */
+        self.map.setCenter(marker.getPosition());
+        model.lastClicked = place.id;
+
+        var flickrUrl = "https://api.flickr.com/services/rest/";
+        var jqxhr = $.ajax(
+            flickrUrl,
+            {
+                data: {
+                    method: 'flickr.photos.search',
+                    api_key: '0c4a27aacfa7626d6870ebd4901b25f5',
+                    format: 'rest',
+                    name: place.name,
+                    lat: place.coord.lat,
+                    lon: place.coord.lng,
+                    radius: 1,
+                    has_geo: 1
+                },
+                timeout: 2000 /* 2 seconds to retrieve or give up */
+            })
+            .done(function (xml) {
+                /*
+                Locate the first photo.  Will iterate through
+                several to generate a carousel of pictures
+                 */
+                var photo = $(xml).find("photo");
+                var i = 0;
+                var carousel = "<div id=\"carousel-example-generic\" class=\"carousel slide\" data-ride=\"carousel\">";
+                carousel += "<div class=\"carousel-inner\" role=\"listbox\">"
+                while (i < 5) {
+                    var farm = photo.attr('farm');
+                    var server = photo.attr('server');
+                    var id = photo.attr('id');
+                    var secret = photo.attr('secret');
+                    var url = "https://farm"
+                        + farm + ".staticflickr.com/"
+                        + server + "/"
+                        + id + "_" + secret + ".jpg";
+
+                    carousel += "<div class=\"item " + (i === 0 ? "active" : "") + " \"><img  class=\"info-picture\" height=\"200px\" src=" + url + " ></div>";
+                    photo = photo.next();
+                    i++;
+                }
+                carousel += "</div><a class=\"left carousel-control\" href=\"#carousel-example-generic\" role=\"button\" data-slide=\"prev\">\n" +
+                    "    <span class=\"glyphicon glyphicon-chevron-left\" aria-hidden=\"true\"></span>\n" +
+                    "    <span class=\"sr-only\">Previous</span>\n" +
+                    "  </a>\n" +
+                    "  <a class=\"right carousel-control\" href=\"#carousel-example-generic\" role=\"button\" data-slide=\"next\">\n" +
+                    "    <span class=\"glyphicon glyphicon-chevron-right\" aria-hidden=\"true\"></span>\n" +
+                    "    <span class=\"sr-only\">Next</span>\n" +
+                    "  </a></div>";
+                var content = '<div id="info-window"><div class="info-place-name">' + place.name + '</div>' + carousel + "</div>";
+                self.infowindow.setContent(content);
+                self.infowindow.open(self.map, marker);
+            })
+            .fail(function () {
+                self.infowindow.setContent('<div class="info-place-name">' + place.name + '</div>(Sorry, we are unable to retrieve pictures at the moment)');
+                self.infowindow.open(self.map, marker);
+            });
+
+        self.bounceMarkerOnce(marker);
+    },
+    
+    /**
+     * Create a marker for the specified place
+     * with the appropriate icon and call the
+     * infowindow setup
+     * @param place
+     */
+    addMarker: function (place) {
+        var self = this;
+        var marker = new google.maps.Marker({
+            position: place.coord,
+            map: self.map,
+            animation: google.maps.Animation.DROP,
+            title: place.name,
+            icon: self.icons[place.type].icon
+        });
+        marker.addListener('click', function () {
+            /**
+             * Check if this marker was the last one we hovered over
+             * If so, don't reanimate it, because that's distracting
+             */
+            if (model.lastClicked !== place.id) {
+                self.createInfoWindow(marker,place);
+            } else {
+                /**
+                 * User has re-clicked the SAME marker...
+                 * Close the info window (since this is probably what they want to do)
+                 */
+                self.infowindow.close();
+                model.lastClicked = undefined;
+            }
+        });
+        self.markerList.push(marker);
+    },
+
+    /**
+     * Create Google Map Markers for supplied places
+     * @param places
+     */
+    createMarkers: function (places) {
         var self = this;
         /**
          *  Remove the existing markers and
@@ -100,93 +234,12 @@ var gmapViewModel = {
          * loop and create a marker with
          * a click listener
          */
-        places.forEach(function (place) {
-            var marker = new google.maps.Marker({
-                position: place.coord,
-                map: self.map,
-                animation: google.maps.Animation.DROP,
-                title: place.name
-            });
-            marker.addListener('click', function () {
-                /**
-                 * Check if this marker was the last one we hovered over
-                 * If so, don't reanimate it, because that's distracting
-                 */
-                if (model.lastClicked !== place.id) {
+        places.forEach(function (place,i) {
+            console.log(i);
+            setTimeout(function() {
+                self.addMarker(place);
+            }, i*200);
 
-                    // self.infowindow.close();
-
-                    self.infowindow.open(self.map, marker);
-                    self.infowindow.setContent('Retrieving data..');
-
-                    model.lastClicked = place.id;
-
-                    console.dir(place);
-                    var flickrUrl = "https://api.flickr.com/services/rest/";
-                    var jqxhr = $.ajax(
-                        flickrUrl,
-                        {
-                            data: {
-                                method: 'flickr.photos.search',
-                                api_key: '0c4a27aacfa7626d6870ebd4901b25f5',
-                                format: 'rest',
-                                name: place.name,
-                                lat: place.coord.lat,
-                                lon: place.coord.lng,
-                                radius: 1,
-                                has_geo: 1
-                            }
-                        })
-                        .done(function (xml) {
-                            var photo = $(xml).find("photo");
-                            var i=0;
-                            var carousel="<div id=\"carousel-example-generic\" class=\"carousel slide\" data-ride=\"carousel\">";
-                            carousel+="<div class=\"carousel-inner\" role=\"listbox\">"
-
-                            while (i<5) {
-
-
-                                var farm = photo.attr('farm');
-                                var server = photo.attr('server');
-                                var id = photo.attr('id');
-                                var secret = photo.attr('secret');
-                                var url="https://farm"
-                                    +farm+".staticflickr.com/"
-                                    +server+"/"
-                                    +id+"_"+secret+".jpg";
-
-                                carousel+="<div class=\"item "+(i===0?"active":"")+" \"><img  class=\"info-picture\" height=\"200px\" src="+url+" ></div>";
-
-                                photo = photo.next();
-                                console.log(photo.attr('id'));
-                                i++;
-                            }
-                            carousel += "</div><a class=\"left carousel-control\" href=\"#carousel-example-generic\" role=\"button\" data-slide=\"prev\">\n" +
-                                "    <span class=\"glyphicon glyphicon-chevron-left\" aria-hidden=\"true\"></span>\n" +
-                                "    <span class=\"sr-only\">Previous</span>\n" +
-                                "  </a>\n" +
-                                "  <a class=\"right carousel-control\" href=\"#carousel-example-generic\" role=\"button\" data-slide=\"next\">\n" +
-                                "    <span class=\"glyphicon glyphicon-chevron-right\" aria-hidden=\"true\"></span>\n" +
-                                "    <span class=\"sr-only\">Next</span>\n" +
-                                "  </a></div>";
-                            console.log(carousel);
-                            // var content = '<div class="info-place-name">'+place.name +'</div><img class="info-picture" src="'+url+'" width="200px">';
-                            var content = '<div id="info-window"><div class="info-place-name">'+place.name +'</div>'+carousel+"</div>";
-                            self.infowindow.setContent(content);
-                            self.infowindow.open(self.map, marker);
-                        })
-                        .fail(function () {
-                            self.infowindow.setContent('<div class="info-place-name">'+place.name +'</div>(Sorry, we are unable to retrieve pictures at the moment)');
-                            self.infowindow.open(self.map, marker);
-                        })
-                        .always(function () {
-                            // alert("complete");
-                        });
-
-                    self.bounceMarkerOnce(marker);
-                }
-            });
-            self.markerList.push(marker);
         });
     },
 
@@ -198,12 +251,13 @@ var gmapViewModel = {
         google.maps.event.trigger(this.markerList[id], 'click');
     },
 
+    /**
+     * Called from index.html once both jQuery and the Google API
+     * map library is ready and is setup in the global scope
+     */
     initMap: function () {
-        /**
-         * Called from index.html once both jQuery and the Google API
-         * map library is ready and is setup in the global scope
-         */
         this.map = new google.maps.Map(document.getElementById('map'), model.mapConfig);
+        this.map.setCenter(model.neighborhoodGeo);
         /**
          * Comment take from Google API Documentation:
          * Best practices: For the best user experience, only one info window should be open on the map at any
@@ -213,6 +267,15 @@ var gmapViewModel = {
          * at the same time
          */
         this.infowindow = new google.maps.InfoWindow({});
+        /**
+         * Once closed, the map should re-center to the original location
+         */
+        var self = this;
+        this.infowindow.addListener('closeclick', function () {
+            // self.map.setCenter({/* This is my neighborhood */ lat: 51.1414, lng: 0.5894});
+            self.map.setCenter(model.neighborhoodGeo);
+
+        });
         /**
          * Once an infowindow is closed, we need to reset lastClick,
          * otherwise if we try to click the same marker again the
@@ -233,7 +296,6 @@ var placeListViewModel = {
 
     placesSearch: ko.observable(),
     placesList: ko.observableArray([]),
-
 
     filterPlaces: function () {
         /**
@@ -294,70 +356,3 @@ $(
         placeListViewModel.init();
     }()
 );
-
-/*
-// flickr
-// 0c4a27aacfa7626d6870ebd4901b25f5
-// https://api.flickr.com/services/rest/?method=flickr.test.echo&name=value
-
-https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=0c4a27aacfa7626d6870ebd4901b25f5&format=rest&name=Leeds+Castle
-
-// &auth_token=72157687008179002-cf819885a41dd5d3&api_sig=2ce67b7e864ede2a266ea886f7f523a3
-// https://api.flickr.com/services/rest/?method=flickr.test.echo&name=value
-
-
-    <photo id="24343289548" owner="42088754@N03" secret="a29473209a" server="4455" farm="5" title="IMG_8485.jpg" ispublic="1" isfriend="0" isfamily="0"/>
-
-    https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
-https://farm5.staticflickr.com/4455/24343289548_a29473209a.jpg
-
-
-    https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=0c4a27aacfa7626d6870ebd4901b25f5&format=rest&name=Leeds+Castle&lat=51.248918&lon=0.630465&has_geo=1
-
-    https://api.twitter.com/1.1/search/tweets.json?q=Leeds+Castle+Kent&result_type=recent&count=4
-
-{lat: 51.248918, lng: 0.630465}),
-
-
-foursquare
-ENDPOINT: venues/X/tips
-PARAMETERS: venueId = 4ef0e7cf7beb5932d5bdeb4e; sort = recent
-
-*/
-
-//
-// https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=0c4a27aacfa7626d6870ebd4901b25f5&format=rest&name=Leeds+Castle&lat=51.248918&lon=0.630465&radius=1&has_geo=1
-//
-//
-//     <photo id="36578225314" owner="29235510@N03" secret="258f6af66b" server="4405" farm="5" title="DSC_8593" ispublic="1" isfriend="0" isfamily="0"/>
-//
-//     https://farm5.staticflickr.com/4405/36578225314_258f6af66b.jpg
-
-// $(document).ready(function(){
-//
-//     $.ajax({
-//         type: "GET",
-//         url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=Jimi_Hendrix&callback=?",
-//         contentType: "application/json; charset=utf-8",
-//         async: false,
-//         dataType: "json",
-//         success: function (data, textStatus, jqXHR) {
-//
-//             var markup = data.parse.text["*"];
-//             var blurb = $('<div></div>').html(markup);
-//
-//             // remove links as they will not work
-//             blurb.find('a').each(function() { $(this).replaceWith($(this).html()); });
-//
-//             // remove any references
-//             blurb.find('sup').remove();
-//
-//             // remove cite error
-//             blurb.find('.mw-ext-cite-error').remove();
-//             $('#article').html($(blurb).find('p'));
-//
-//         },
-//         error: function (errorMessage) {
-//         }
-//     });
-// });
